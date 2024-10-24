72c98fd7787168d99f7039a56dc7bdd1d0ca67ac
Lucas Gabriel Gómez <lucasgabrielg2001@gmail.com>
2024-10-24 12:46:58 -0300

    Tailwind

---
+++ src/components/projects/ProjectModal.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+export default function ProjectModal({ isOpen, toggleModal, project }) {
+  if (!isOpen) return null; 
+
+  return (
+    <div id="portfolioModal1" className="fixed inset-0 z-50 overflow-hidden">
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="relative bg-white rounded-lg shadow-lg max-w-4xl w-full">
+          <div className="flex justify-end p-4 border-b">
+            <button className="text-gray-500 hover:text-gray-700" onClick={toggleModal}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+          </div>
+
+          <div className="p-6 text-center">
+            <h2 className="text-2xl font-bold text-gray-800 uppercase mb-4">{project.title}</h2>
+
+            <div className="flex justify-center items-center mb-6">
+              <div className="w-1/4 h-px bg-gray-400"></div>
+              <div className="px-4 text-yellow-500">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 24 24" fill="currentColor">
+                  <path fillRule="evenodd" d="M12 17.75l-6.472 3.404 1.236-7.207-5.244-5.103 7.236-1.051L12 0l3.244 7.793 7.236 1.051-5.244 5.103 1.236 7.207L12 17.75z" />
+                </svg>
+              </div>
+              <div className="w-1/4 h-px bg-gray-400"></div>
+            </div>
+
+            <img className="w-full h-48 md:h-36 sm:h-24 object-contain object-center" src={project.image} alt={project.title} />
+
+            <p className="text-gray-600 mb-6">{project.description}</p>
+
+            <div className="space-x-4">
+              <a
+                href={project.codeLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition duration-300 inline-flex items-center"
+              >
+                Ver Código
+              </a>
+              {project.webLink &&
+                <a
+                    href={project.webLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition duration-300 inline-flex items-center"
+                >
+                    Ver Página Web
+                </a>
+              }
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import django from '../../assets/django.svg';
+import java from '../../assets/java.svg';
+import spring from '../../assets/spring.svg';
+import react from '../../assets/react.svg';
+import ProjectModal from './ProjectModal';
+
+export default function Projects() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentProject, setCurrentProject] = useState(null);
+
+  const listProjects = [
+    {
+        id: 1,
+        image: django,
+        title: 'Blog DeFi',
+        description: 'El objetivo de este proyecto fue crear una aplicación web utilizando el framework Django y aplicando los conocimientos adquiridos durante el curso. El blog permitirá a los visitantes leer artículos, filtrar el contenido por categorías, antigüedad y orden alfabético. Los usuarios registrados, además, podrán comentar en los artículos, editar y eliminar sus comentarios. Los usuarios colaboradores podrán crear, editar y eliminar artículos, categorías y comentarios.',
+        codeLink: 'https://github.com/matheosc/g5-blog',
+        webLink: 'https://triclion01.pythonanywhere.com',
+    },
+    {
+        id: 2,
+        image: java,
+        title: 'Sistema Bancario',
+        description: 'El objetivo de este proyecto fue crear el back-end que simule un sistema bancario. Un cliente del banco puede tener múltiples cuentas. Los clientes pueden gestionar sus cuentas y consultar el saldo total. Las cuentas bancarias permiten depositar, retirar y consultar saldo. Hay dos tipos de cuentas: de ahorro, que generan intereses, y corriente, con posibilidad de sobregiro. El banco permitiende registrar nuevos clientes y abrir cuentas.',
+        codeLink: 'https://github.com/L-byte-dev/sistemabancario',
+    },
+    {
+        id: 3,
+        image: spring,
+        title: 'Streaming Música',
+        description: 'El objetivo de este proyecto fue crear una aplicación back end usando el framework Spring Boot y una base de datos. La aplicación permite a los usuarios explorar una biblioteca musical, realizar búsquedas avanzadas por título, género, artista o álbum, y descubrir nuevas canciones basadas en sus preferencias. Los usuarios pueden crear perfiles con listas de reproducción personalizadas, gestionarlas (añadir/eliminar canciones, hacerlas públicas, repetir o reproducir aleatoriamente) y compartirlas con otros',
+        codeLink: 'https://github.com/L-byte-dev/trabajopracticospring',
+    },
+    {
+        id: 4,
+        image: react,
+        title: 'Youtube Music Podcasts',
+        description: 'El objetivo de este proyecto fue crear una aplicación con un diseño inspirado en Youtube Music. Se usa un custom hook para consumir la API audioboom y obtener los datos de los podcasts. La aplicación permite que al seleccionar un podcast se despliegue una barra de reproducción en la cual se puede pausar y adelantar o retroceder al audio, además permite crear, con un formulario, listas de reproducción, las cuales se muestra en tiempo real, ',
+        codeLink: 'https://github.com/L-byte-dev/podcast-player',
+        webLink: 'https://podcast-player-lake.vercel.app',
+    },
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 780,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
+  const toggleModal = (project) => {
+    setIsModalOpen(!isModalOpen);
+    setCurrentProject(project);
+  };
+
+  return (
+    <section data-aos='fade-right' data-aos-delay='100' id="projects" className="relative overflow-hidden flex flex-col text-white body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <h2 className="text-4xl font-bold text-center mb-12">Mis Proyectos</h2>
+        <Slider {...settings}>
+          {listProjects.map((project) => (
+            <div key={project.id} className="p-4">
+              <div className="h-full border-2 border-orange-400 shadow-[0_0_15px_rgba(255,165,0,0.7)] border-opacity-60 rounded-lg overflow-hidden">
+                <img
+                  src={project.image}
+                  alt={project.title}
+                  className="w-full h-48 md:h-36 sm:h-24 object-contain object-center cursor-pointer"
+                  onClick={() => toggleModal(project)} 
+                />
+                <div className="p-6">
+                  <h2 className="tracking-widest text-xl title-font font-medium text-gray-400 mb-1">
+                    {project.title}
+                  </h2>
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
+        <ProjectModal isOpen={isModalOpen} toggleModal={() => toggleModal(null)} project={currentProject} />
+      </div>
+    </section>
+  );
+}
